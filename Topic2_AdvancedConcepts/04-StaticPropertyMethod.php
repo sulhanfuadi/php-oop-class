b0d1661f8dddc5f31ae970b9aa189b962fd56651
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-05 17:59:14 +0700

    feat: add explanation for static properties and methods
    Added explanations for static properties and methods in OOP. Described how static properties and methods can be accessed without creating a new object, using the 'static' keyword. Provided syntax examples for accessing static properties and methods, and mentioned the use of encapsulation with static properties and methods.

---
+++ Topic2_AdvancedConcepts/04-StaticPropertyMethod.php
@@ -0,0 +1,31 @@
+<?php
+
+// static property and method
+
+// Static property merupakan properti yang bisa dipanggil tanpa membuat object baru.
+
+// perilaku yang dimiliki pada class pada umumnya
+// tidak terikat dengan instance tertentu bisa dipanggil secara langsung tanpa instance
+// menggunakan keyword static
+
+// sintaks berbeda untuk mengakses property dan method static
+// Student::$angka, Student::halo(), bukan $this->angka atau $this->halo()
+// static tidak bisa menggunakan $this
+// instead, menggunakan self; self::$angka, self::halo()
+// bisa digabungkan dengan enskapsulasi, public static, private static, protected static
+
+class Student
+{
+  public static $angka = 1; // static property, bisa diakses tanpa instance
+
+  public static function halo() // static method, bisa diakses tanpa instance
+  {
+    return "Halo " . self::$angka++ . " kali.";
+  }
+}
+
+// cara mengakses static property dan method
+// menggunakan class name, bukan object
+echo Student::halo(); // Halo 1 kali.
+echo Student::halo(); // Halo 2 kali.
+echo Student::$angka; // 2
